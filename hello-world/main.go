bf342649895b3cefff052c902ceeeef09f5cda0e
Ryan Castner <castner.rr@gmail.com>
2020-01-17 09:43:19 -0500

    refactor hello world

--- hello-world/main.go
+++ hello-world/main.go
@@ -2,10 +2,10 @@ package main
 
 import "fmt"
 
-func Hello() string {
-	return "Hello, world"
+func Hello(str string) string {
+	return "Hello, " + str
 }
 
 func main() {
-	fmt.Println(Hello())
+	fmt.Println(Hello("world"))
 }
