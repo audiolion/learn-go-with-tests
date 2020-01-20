11d5b64c662c6ea651f048fee9b15f65e72b97ea
Ryan Castner <castner.rr@gmail.com>
2020-01-20 13:17:29 -0500

    arrays and slices part 2

--- arrays-and-slices/sum.go
+++ arrays-and-slices/sum.go
@@ -1,10 +1,22 @@
 package arrays_and_slices
 
-// Sum sums a variable number of arguments given as input
+// Sum sums a slice of ints, returning a single int
 func Sum(nums []int) int {
 	var total int = 0
 	for _, n := range nums {
 		total += n
 	}
 	return total
+}
+
+// SumAll computes the sum of each slice passed in as input
+// returning the result in a slice of ints where the index
+// corresponds to the positional ordering of the int slice
+// passed as input
+func SumAll(nums ...[]int) []int {
+	result := make([]int, len(nums))
+	for i, n := range nums {
+		result[i] = Sum(n)
+	}
+	return result
 }
