e2a3274e01c622ee754156606994e445081d6d53
Ryan Castner <castner.rr@gmail.com>
2020-01-22 12:36:49 -0500

    structs methods interfaces part 2

--- structs-methods-interfaces/shapes.go
+++ structs-methods-interfaces/shapes.go
@@ -1,13 +1,45 @@
 package structs_methods_interfaces
 
-// Perimeter calculates the perimeter of a rectangle
-// given the width and height.
-func Perimeter(x float64, y float64) float64 {
-	return 2 * (x + y)
+import "math"
+
+type Shape interface {
+	Area() float64
 }
 
-// Area calculates the area of a rectangle
-// given the width and height.
-func Area(x float64, y float64) float64 {
-	return x * y
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.Width + r.Height)
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Circumference() float64 {
+	return math.Pi * c.Diameter()
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * math.Pow(c.Radius, 2)
+}
+
+func (c Circle) Diameter() float64 {
+	return c.Radius * 2
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) / 2
 }
--- structs-methods-interfaces/shapes_test.go
+++ structs-methods-interfaces/shapes_test.go
@@ -2,8 +2,30 @@ package structs_methods_interfaces
 
 import "testing"
 
-func TestPerimeter(t *testing.T) {
-	got := Perimeter(10.0, 10.0)
+func TestArea(t *testing.T) {
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{12, 6}, 72.0},
+		{Circle{10}, 314.1592653589793},
+		{Triangle{12, 6}, 36.0},
+	}
+
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("%#v got %g want %g", tt.shape, got, tt.want)
+		}
+	}
+}
+
+func TestRectangle_Perimeter(t *testing.T) {
+	rectangle := Rectangle{
+		Width:  10.0,
+		Height: 10.0,
+	}
+	got := rectangle.Perimeter()
 	want := 40.0
 
 	if got != want {
@@ -11,11 +33,47 @@ func TestPerimeter(t *testing.T) {
 	}
 }
 
-func TestArea(t *testing.T) {
-	got := Area(10.0, 10.0)
+func TestRectangle_Area(t *testing.T) {
+
+	rectangle := Rectangle{
+		Width:  10.0,
+		Height: 10.0,
+	}
+	got := rectangle.Area()
 	want := 100.0
 
 	if got != want {
 		t.Errorf("got %.2f want %.2f", got, want)
+	}
+
+}
+
+func TestCircle_Area(t *testing.T) {
+	circle := Circle{10}
+	got := circle.Area()
+	want := 314.1592653589793
+
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
+
+func TestCircle_Circumference(t *testing.T) {
+	circle := Circle{10}
+	got := circle.Circumference()
+	want := 62.83185307179586
+
+	if got != want {
+		t.Errorf("got %g want %g", got, want)
+	}
+}
+
+func TestCircle_Diameter(t *testing.T) {
+	circle := Circle{10}
+	got := circle.Diameter()
+	want := 20.0
+
+	if got != want {
+		t.Errorf("got %.2f want %2.f", got, want)
 	}
 }
