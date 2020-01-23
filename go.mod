fb42a9d3cff0f841611dac6ddd7c58e73577d778
Ryan Castner <castner.rr@gmail.com>
2020-01-22 19:23:21 -0500

    pointers errors

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/audiolion/learn-go-with-tests
 
 go 1.13
+
+require (
+	github.com/kisielk/errcheck v1.2.0 // indirect
+	golang.org/x/tools v0.0.0-20200122220014-bf1340f18c4a // indirect
+)
---
+++ pointers-errors/wallet.go
@@ -0,0 +1,34 @@
+package pointers_errors
+
+import (
+	"errors"
+	"fmt"
+)
+
+type Bitcoin int
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Deposit(amt Bitcoin) {
+	w.balance += amt
+}
+
+var ErrInsufficientFunds = errors.New("cannot withdraw, insufficient funds")
+
+func (w *Wallet) Withdraw(amt Bitcoin) error {
+	if amt > w.balance {
+		return ErrInsufficientFunds
+	}
+	w.balance -= amt
+	return nil
+}
+
+func (w Wallet) Balance() Bitcoin {
+	return w.balance
+}
