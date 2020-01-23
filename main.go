a26ebd3e5401b3f5e9b9c0343a9c49dc2195ddbc
Ryan Castner <castner.rr@gmail.com>
2020-01-22 21:38:51 -0500

    dependency injection

--- main.go
+++ main.go
@@ -1,5 +1,8 @@
 package main
 
-func main() {
+import "net/http"
+import "github.com/audiolion/learn-go-with-tests/dependency-injection"
 
+func main() {
+	http.ListenAndServe(":5000", http.HandlerFunc(dependency_injection.MyGreeterHandler))
 }
